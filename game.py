17aca09590184b300cce2028e200b067e733ae5b
AndreaScacchi <andreascacchi10@gmail.com>
2022-08-14 11:19:16 +0200

    return statement added

--- game.py
+++ game.py
@@ -5,3 +5,4 @@ def get_choices():
     options = ["rock", "paper", "scissors"]
     computer_choice = random.choice(options)
     choices = {"player": player_choice, "computer": computer_choice}
+    return choices
