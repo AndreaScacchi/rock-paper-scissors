14b7c187198690b9a6ad7dbaf563aaaec997bf91
AndreaScacchi <andreascacchi10@gmail.com>
2022-08-14 11:33:30 +0200

    check_win function implemented and result variable declared

--- game.py
+++ game.py
@@ -9,3 +9,25 @@ def get_choices():
 
 def check_win(player, computer):
     print(f"You chose {player}, computer chose {computer}")
+
+    if player == computer:
+        return "It's a tie!"
+    elif player == "rock":
+        if computer == "scissors":
+            return "Rock smashes scissors! You win!"
+        else:
+            return "Paper covers rock! You lose!"
+    elif player == "paper":
+        if computer == "rock":
+            return "Paper covers rock! You win!"
+        else:
+            return "Scissors cuts paper! You lose!"
+    elif player == "scissors":
+        if computer == "paper":
+            return "Scissors cuts paper! You win!"
+        else:
+            return "Rock smashes scissors! You lose!"
+
+choices = get_choices();
+result = check_win(choices["player"], choices["computer"])
+print(result)
