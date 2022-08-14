f5f7490169a1e0912a714d0303db68c26a67c553
AndreaScacchi <andreascacchi10@gmail.com>
2022-08-14 11:17:16 +0200

    computer_choice variable initialized with the random libraries to take random option from options list

--- game.py
+++ game.py
@@ -3,3 +3,4 @@ import random
 def get_choices():
     player_choices = input("Enter a choice (rock, paper, scissors)")
     options = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(options)
